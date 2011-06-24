df45ceb9794c41dcdb2a7bae81d77571d043f347
Philippe Le Van <philippe.leva@kitpages.fr>
2011-06-24 02:38:48 +0200

    small license modification, I should add a clear BSD or MIT license...

--- KitpagesSimpleCacheBundle.php
+++ KitpagesSimpleCacheBundle.php
@@ -1,9 +1,7 @@
 <?php
 
 /*
- * This file is part of the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * Philippe Le Van (@plv) http://www.kitpages.fr
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
